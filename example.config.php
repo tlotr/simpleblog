7e0375f93f0f092030a649a358cbddf7c9a4b874
roun512 <roun.miler@gmail.com>
2015-04-12 09:01:15 +0300

    Added config.php

---
+++ example.config.php
@@ -0,0 +1,10 @@
+<?php
+$config['domain'] = "http://localhost"; // Change it to your website URL
+$config['name'] = "simpleBlog"; // Change it to your website name
+
+$config['db']['host'] = "127.0.0.1"; // Server of your database
+$config['db']['username'] = "root"; // Username of your database
+$config['db']['password'] = "toor"; // Password of your database
+$config['db']['database'] = "my_blog"; // The name of the database default: my_blog 
+
+?>
